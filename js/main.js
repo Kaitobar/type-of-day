7d2b7062bd14d1abe83a632c330e2d99cb703543
Katherin Lasso <lassokatherin@gmail.com>
2022-02-24 18:21:12 -0500

    Modificado para que acepte mayusculas y minusculas

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase()
 
-    if (day === "Wednesday" || day === "Thursday" ) {
+    if (day === "tuesday" || day === "thursday" ) {
         alert("Studyyyy Dayyy")
-    } else if (day === "Monday") {
-        alert("BOORING DAAAAY")
-    } else {
+    } else if (day === "saturday" || day === "sunday") {
         alert("Weekeeend HURRA!")
+    } else {
+        alert("BORINGGG")
     }
 
 
